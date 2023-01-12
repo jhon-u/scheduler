b4c742419bca1be95ffc9619948aa5c964448942
Jhon Useche <jhon.useche@telus.com>
2023-01-11 20:22:23 -0700

    Update the Appointment component to use the useVisualMode hook.

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,18 +3,36 @@ import "components/Appointment/styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
+import useVisualMode from "../../hooks/useVisualMode";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ? (
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
         <Show
           name={props.interview.student}
           interviewer={props.interview.interviewer}
+          onEdit={() => console.log("Clicked onEdit")}
+          onDelete={() => console.log("Clicked onDelete")}
         />
-      ) : (
-        <Empty />
+      )}
+      {mode === CREATE && (
+        <Form
+          interviewers={[]}
+          onCancel={() => back()}
+          onSave={console.log("Clicked onSave")}
+        />
       )}
     </article>
   );
