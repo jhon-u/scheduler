caaf952136114d2431670bf0cb78df00950af38f
Jhon Useche <jhon.useche@telus.com>
2023-01-18 14:20:21 -0700

    Install Cypress and added navigation.spec.js.

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,18 +4,23 @@ import classNames from "classnames";
 
 export default function DayListItem(props) {
   const dayClass = classNames("day-list__item", {
-    'day-list__item--selected': props.selected,
-    'day-list__item--full': !props.spots
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": !props.spots,
   });
 
   const formatSpots = (spots) => {
     if (spots === 0) return "no spots remaining";
     if (spots === 1) return `1 spot remaining`;
     return `${spots} spots remaining`;
-  }
+  };
 
   return (
-    <li className={dayClass} onClick={() => props.setDay(props.name)} selected={props.selected} data-testid="day">
+    <li
+      className={dayClass}
+      onClick={() => props.setDay(props.name)}
+      selected={props.selected}
+      data-testid="day"
+    >
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
